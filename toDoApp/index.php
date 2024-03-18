1e4b47de84b89e065b88189d02f24e41fe60e46e
Martin Pospíšil <martasek.pospisil@gmail.com>
2024-03-18 18:46:57 +0100

    Added php file

--- toDoApp/index.php
+++ toDoApp/index.php
@@ -1,3 +1,12 @@
 <?php
+    //SETUP CONNECTION
+    $mysqli = new mysqli("127.0.0.1", "root", "", "toDoApp");
+
+    //CHECK CONNECTION
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
     
+    var_dump($mysqli);
 ?>
